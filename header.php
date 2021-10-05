74bc7396c77e8c8e3b311c45e1c136067fb20bae
Jaume Subirats <jaume.subirats@gmail.com>
2021-10-05 14:35:32 +0200

    added assest and media queries

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+        <div id="logo"><img src="img/logo.svg" ></div>
+        <div id="rrss">
+            <ul>
+                <li> <i class="bi bi-twitter"></i>   </li>
+                <li> <i class="bi bi-instagram"></i> </li>
+                <li> <i class="bi bi-linkedin"></i>  </li>
+            </ul>
+        </div>
+
+</header>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'head.php' ?>
+
+<body>
+
+<div class="page">
+
+<?php include 'header.php' ?>
+
+
+    <nav>
+        <ul>
+            <li>opció A</li>
+            <li>opció B</li>
+            <li>opció C</li>
+            <li>opció D</li>
+            <li>opció E</li>
+        </ul>
+    </nav>
+
+    <?php include 'aside.php' ?>
+
+    <?php include 'main.php' ?>
+
+
+<?php include 'footer.php' ?>
+
+</div>
+</body>
+</html>
